2044961e8f7b349965dbde8c8fa4d81936ed165c
Alvaro <alvaroalanis123@gmail.com>
2020-06-26 23:20:17 -0400

    Im sorry i forgot to switch branches! Milestone 5 and 6

---
+++ routes/user.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+const Users = require("../model/user_model");
+var ObjectId = require('mongodb').ObjectId;
+
+
+router.get('/user/viewUser/:id',function (req,res,next) {
+
+
+    Users.find({"_id" : ObjectId(req.params.id)}, function (err,doc){
+
+        res.render('user', {user: doc[0]});
+    });
+
+
+
+
+})
+
+router.delete('/:id', function (req, res, next) {
+
+    const id = req.params.id;
+
+
+    Users.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/listUser'});
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+
+});
+
+
+
+module.exports = router;
